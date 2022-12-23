8cd3ce946bcd61e7090bf187c78329fe3646ae95
mirakzen <mirakzen@gmail.com>
2022-12-23 21:26:03 +0300

    Initial commit with some code

---
+++ app/main.py
@@ -0,0 +1,40 @@
+from argparse import ArgumentParser
+
+import uvicorn
+from fastapi import FastAPI
+from utils.config import YamlConfigManager, cfg
+
+ConfigManager = YamlConfigManager(interval=60)
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup():
+    await ConfigManager.start(cfg)
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    pass
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="FastAPI backend")
+    parser.add_argument(
+        "-H", "--Host", required=True, action="store", dest="host", help="Server host"
+    )
+    parser.add_argument(
+        "-P", "--Port", required=True, action="store", dest="port", help="Server port"
+    )
+
+    args = parser.parse_args()
+
+    log_config = uvicorn.config.LOGGING_CONFIG
+    log_config["formatters"]["access"][
+        "fmt"
+    ] = '%(asctime)s - %(client_addr)s - "%(request_line)s" %(status_code)s'
+
+    uvicorn.run(
+        app, host=args.host, port=int(args.port), log_config=log_config, reload=False
+    )
---
+++ app/utils/config.py
@@ -0,0 +1,48 @@
+import asyncio
+from types import SimpleNamespace
+
+import yaml
+from aiofile import async_open
+
+
+class YamlConfigManager:
+    def __init__(self, interval):
+        self._update_interval = interval
+        self._config_file = "config.yaml"
+
+    async def _update_loop(self, config):
+        while True:
+            try:
+                await self._update(config)
+            except Exception as e:
+                print(f"Failed to update config, see you next time \n{repr(e)}")
+            await asyncio.sleep(self._update_interval)
+
+    async def _init(self, config):
+        async with async_open(self._config_file, "r") as f:
+            data = yaml.safe_load(await f.read())
+
+            database = data["database"]
+            config.DB_CONNECTION_STRING = f"postgresql+asyncpg://{database['user']}:{database['password']}@{database['host']}:{database['port']}/{database['database']}"
+
+            admin = data["admin"]
+            config.ADMIN_LOGIN = admin["login"]
+            config.ADMIN_PASSWORD = admin["password"]
+
+    async def _update(self, config):
+        async with async_open(self._config_file, "r") as f:
+            data = yaml.safe_load(await f.read())
+
+            config.DOMAIN = data["domain"]
+            self._update_interval = data["update_interval"]
+
+            security = data["security"]
+            config.TOKEN_SECRET_KEY = security["token_secret_key"]
+            config.TOKEN_NAME = security["token_name"]
+
+    async def start(self, config):
+        self._update_task = asyncio.ensure_future(self._update_loop(config))
+        await self._init(config)
+
+
+cfg = SimpleNamespace()
